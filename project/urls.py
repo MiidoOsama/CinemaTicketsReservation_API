aec1ae68ea78f3b60901c8862e9f40da357edb67
MiidoOsama <xmiido@yandex.com>
2024-03-02 11:55:29 +0200

    finish

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,44 @@
 from django.contrib import admin
 from django.urls import path, include
 from tickets import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('guests' , views.viewsets_guest)
+router.register('movies' , views.viewsets_movie)
+router.register('reservations' , views.viewsets_reservation)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path('api/', include('rest_framework.urls'))
     path("django/jsonresponsenomodel/", views.no_rest_no_model),
-    #2
+    # 2
     path("django/jsonresponsefrommodel/", views.no_rest_from_model),
-    #3 GET POST from rest framework function  based view @api_view
-    path("rest/fbv/",views.FBV_List),
-    #3.1 GET PUT DELETE from rest framework function  based view @api_view
-    path("rest/fbv/<int:pk>",views.FBV_pk),
+    # 3 GET POST from rest framework function  based view @api_view
+    path("rest/fbv/", views.FBV_List),
+    # 3.1 GET PUT DELETE from rest framework function  based view @api_view
+    path("rest/fbv/<int:pk>", views.FBV_pk),
+    # 4 GET POST from rest framework class based view APIView
+    path("rest/cbv/", views.CBV_List.as_view()),
+    # 4.1 GET PUT DELETE from rest framework class based view APIView
+    path("rest/cbv/<int:pk>", views.CBV_pk.as_view()),
+
+    # 5 GET POST from rest framework class based view mixins
+    path("rest/mixins/", views.mixins_list.as_view()),
+    # 5.2 GET PUT DELETE from rest framework class based view mixins
+    path("rest/mixins/<int:pk>", views.mixins_pk.as_view()),
+
+    # 6 GET POST from rest framework class based view generics
+    path("rest/generics/", views.generics_list.as_view()),
+    # 5.2 GET PUT DELETE from rest framework class based view generics
+    path("rest/generics/<int:pk>", views.generics_pk.as_view()),
+
+    #7 Viewsets
+    path("rest/viewsets/", include(router.urls)),
+
+    #8 Find movie 
+    path('fbv/findmovie/', views.find_movie),
+
+    #9 new reservation 
+    path('fbv/newreservation/', views.new_reservation)
 ]
--- tickets/models.py
+++ tickets/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Movie(models.Model):
     hall = models.CharField(max_length=10)
     movie = models.CharField(max_length=50)
-    date = models.DateField()
+    # date = models.DateField()
     
     def __str__(self):
         return self.movie
@@ -25,7 +25,6 @@ class Reservation(models.Model):
     guest = models.ForeignKey(Guest, related_name= 'reservation' ,on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, related_name= 'reservation' ,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.guest
+
 
 
